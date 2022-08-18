5698477a5c0ca5f86ff0e5b4cabed3d7b2a75d16
null-Exception1 <82306174+null-Exception1@users.noreply.github.com>
2022-08-18 18:18:10 +0530

    Update eh.py

--- eh.py
+++ eh.py
@@ -2,10 +2,10 @@ import discum
 import time
 import random
 bot = discum.Client(token='NzY5NTI5MzM3ODU3ODM1MDE5.GSbwgs.edrcq8ruNFQ6jldMEuj6ISBcfz5lD1mhj_a8RU', log=False)
-channel = "1001820243682934855"
+channel = "779279804297248769"
 while True:
     print(time.ctime())
-    if "12:47:00" in time.ctime() or "12:47:01" in time.ctime() or "12:47:02" in time.ctime():
+    if "12:48:00" in time.ctime() or "12:48:01" in time.ctime() or "12:48:02" in time.ctime():
         bot.sendMessage(channel, random.choice(["washed my eyes","eyes washed just now","just washed my eyes"]))
     if "00:28:00" in time.ctime():
         bot.sendMessage(channel, random.choice(["washed my eyes","eyes washed just now","just washed my eyes"]))
