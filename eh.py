bdc064902b645f28f6bd6febf2d9d586af6abc49
null-Exception1 <82306174+null-Exception1@users.noreply.github.com>
2022-08-19 23:37:31 +0530

    Update eh.py

--- eh.py
+++ eh.py
@@ -1,19 +1,40 @@
-import discum
-import time
-import random
-bot = discum.Client(token='NzY5NTI5MzM3ODU3ODM1MDE5.GSbwgs.edrcq8ruNFQ6jldMEuj6ISBcfz5lD1mhj_a8RU', log=False)
-channel = "1001820243682934855"
-a = False
+import datetime
+from Google import Create_Service
+from googleapiclient.http import MediaFileUpload
+import time 
+import os    
 while True:
-    if "12:59:00" in time.ctime() or "12:59:01" in time.ctime() or "12:59:02" in time.ctime() and a == False:
-        a = True
-        bot.sendMessage(channel, random.choice(["washed my eyes","eyes washed just now","just washed my eyes"]))
-    else:
-        a = False
-    if "00:28:00" in time.ctime() or "00:28:01" in time.ctime() or "00:28:02" in time.ctime() and a == False:
-        a = True
-        bot.sendMessage(channel, random.choice(["washed my eyes","eyes washed just now","just washed my eyes"]))
-    else:
-        a = False
-    
-    time.sleep(1)
+    for i in os.listdir('openings/videos'):
+        print("Publishing :",i)
+        CLIENT_SECRET_FILE = 'client_secrets.json'
+        API_NAME = 'youtube'
+        API_VERSION = 'v3'
+        SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
+
+        service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+
+        upload_date_time = datetime.datetime(2020, 12, 25, 12, 30, 0).isoformat() + '.000Z'
+
+        request_body = {
+            'snippet': {
+                'categoryI': 19,
+                'title': i[:-4],
+                'description': '#shorts #chess #openings #chessopenings #learnchessonline #chesstutorials #chesscom #chessrush #chesspuzzle #chessvideoplus #openingsequence #chessopeningtricks #chessopeningtraps #chessbaseindia #chesslover',
+                'tags': ['shorts','chess','openings','chessopenings','learnchessonline','chesstutorials','chesscom','chessrush','chesspuzzle','chessvideoplus','openingsequence','chessopeningtricks','chessopeningtraps']
+            },
+            'status': {
+                'privacyStatus': 'public',
+                'selfDeclaredMadeForKids': False, 
+            },
+            'notifySubscribers': True
+        }
+
+        mediaFile = MediaFileUpload('openings/videos/'+i)
+
+        response_upload = service.videos().insert(
+            part='snippet,status',
+            body=request_body,
+            media_body=mediaFile
+        ).execute()
+
+        time.sleep(1800)
